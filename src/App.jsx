c2769c5052908ee5b51a1e44cc0112745ffc49a5
saraswathy <saraswathychandra88@gmail.com>
2025-07-06 07:16:15 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Loading from "./components/Loading.jsx";
+import "./index.css";
+const App = () => {
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
+};
+export default App;
