2866bd9e94020aad9c0ebc3d45e43d61ff779af4
saraswathy <saraswathychandra88@gmail.com>
2025-07-06 10:36:56 +0200

    Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import Loading from "./components/Loading.jsx";
 import "./index.css";
 const App = () => {
-  return (
-    <>
-      <Loading></Loading>
-    </>
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return <>{!isLoaded && <Loading onComplete={() => setIsLoaded(true)} />}</>;
 };
 export default App;
