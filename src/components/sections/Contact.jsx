25d3a9330590d9b9d26ae30ab79cb5b266a186f8
saraswathy <saraswathychandra88@gmail.com>
2025-07-07 23:09:05 +0200

    contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import RevealOnScroll from "../RevealOnScroll";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const SERVICE_ID = "service_kyilx3y";
+  const TEMPLATE_ID = "template_4ijr5h6";
+  const PUBLIC_KEY = "qES8Z2V6PykMF9-tp";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .then(() => {
+        alert("Message Sent!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("oops! something went wrong.Please try again"));
+  };
   return (
     <section
       id="contact"
@@ -14,15 +34,19 @@ const Contact = () => {
           >
             Get In Touch
           </h2>
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="realtive">
               <input
                 type="text"
                 id="name"
                 name="name"
                 required
+                value={formData.name}
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="Name.."
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
               ></input>
             </div>
             <div className="realtive">
@@ -30,19 +54,27 @@ const Contact = () => {
                 type="email"
                 id="email"
                 name="email"
+                value={formData.email}
                 required
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="example.gmail.com"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
               ></input>
             </div>
             <div className="realtive">
               <textarea
                 id="message"
                 name="message"
+                value={formData.message}
                 required
                 rows={5}
                 className="w-full  bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="Your message"
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
               ></textarea>
             </div>
             <button
