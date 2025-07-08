a1d4db41d7fdab90b6432712c9ba984459dc5225
saraswathy <saraswathychandra88@gmail.com>
2025-07-08 10:42:41 +0200

    deploy settings

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -14,7 +14,12 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     emailjs
-      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
       .then(() => {
         alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
